2bec32876924cd865b480f0426971f6867a045a7
Orbitix Solutions <162249501+OrbitixSolutions@users.noreply.github.com>
2024-03-31 19:42:33 -0500

    Fixing route team/full #1

--- src/app/api/teams/full/route.ts
+++ src/app/api/teams/full/route.ts
@@ -1,16 +1,20 @@
-import { NextResponse } from "next/server"
-import prisma from "@/libs/prisma"
+import { NextResponse } from 'next/server'
+import prisma from '@/libs/prisma'
 
 export async function GET(request: Request) {
-    const teams = await prisma.team.findMany({
-      orderBy: {
-        createdDate: 'desc'
-      },
-      include: {
-        players: true,
-        teamStats: true,
-        playerStats: true
-      }
-    })
-    return NextResponse.json(teams, { status: 200 })
-  }
+  const teams = await prisma.team.findMany({
+    orderBy: {
+      createdDate: 'desc'
+    },
+    include: {
+      players: true,
+      teamStats: true,
+      playerStats: true
+    }
+  })
+  return NextResponse.json(teams, { status: 200 })
+}
+
+export async function POST(request: Request) {
+  return NextResponse.json('Success', { status: 200 })
+}
