e35f2b847d214f442bea8d672932eac178aeb9ad
Miguel AJM <koekun3000@gmail.com>
2024-04-28 19:48:07 -0500

    Cambios menores

--- src/components/dates/image/images-matches-keys.tsx
+++ src/components/dates/image/images-matches-keys.tsx
@@ -39,7 +39,7 @@ const ImagesMatchesKeys = ({ match }: { match: ExtendedMatch }) => {
         <Tooltip
           content={
             <div className='flex flex-col gap-2 text-center'>
-              <p>Name: {teamKeyB.name}</p>
+              <p>{teamKeyB.name}</p>
             </div>
           }
         >
