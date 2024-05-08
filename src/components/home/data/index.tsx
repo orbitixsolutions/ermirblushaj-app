8a43507f6cd2405225e1535868fa9158519ef814
Miguel AJM <koekun3000@gmail.com>
2024-05-08 02:22:00 -0500

    Cambios menores en el footer

---
+++ src/components/home/data/index.tsx
@@ -0,0 +1,43 @@
+import {
+  IconBrandFacebookFilled,
+  IconBrandInstagram,
+  IconBrandTiktokFilled,
+  IconBrandWhatsapp
+} from '@tabler/icons-react'
+
+export const social = [
+  {
+    icon: <IconBrandFacebookFilled />,
+    text: 'Social Media'
+  },
+  {
+    icon: <IconBrandInstagram />,
+    text: 'Social Media'
+  },
+  {
+    icon: <IconBrandTiktokFilled />,
+    text: 'Social Media'
+  },
+  {
+    icon: <IconBrandWhatsapp />,
+    text: 'Social Media'
+  }
+]
+
+export const phonesNumbers = [
+  {
+    number: '3491242172'
+  },
+  {
+    number: '3491242172'
+  }
+]
+
+export const streets = [
+  {
+    street: 'Crra 42 #### San isidro'
+  },
+  {
+    street: 'Crra 42 #### San isidro'
+  }
+]
--- src/components/home/footer.tsx
+++ src/components/home/footer.tsx
@@ -1,12 +1,7 @@
 import { BrandLogo } from '@/assets/images'
 import { Image } from '@nextui-org/react'
-import {
-  IconBrandFacebookFilled,
-  IconBrandInstagram,
-  IconBrandTiktok,
-  IconBrandWhatsapp
-} from '@tabler/icons-react'
 import { useTranslations } from 'next-intl'
+import { phonesNumbers, social, streets } from './data'
 
 const Footer = () => {
   const content = useTranslations('Footer')
@@ -26,52 +21,27 @@ const Footer = () => {
         <div className='col-span-8 xs:col-span-4 md:col-span-2 space-y-4'>
           <h3 className='text-xl font-bold'>{content('social.title')}</h3>
           <ol className='flex flex-col space-y-4'>
-            <li className='flex items-center gap-2'>
-              <IconBrandFacebookFilled />{' '}
-              <span className='text-custom-lightgray/80 text-xs md:text-lg'>
-                Social Media
-              </span>
-            </li>
-            <li className='flex items-center gap-2'>
-              <IconBrandInstagram />{' '}
-              <span className='text-custom-lightgray/80 text-xs md:text-lg'>
-                Social Media
-              </span>
-            </li>
-            <li className='flex items-center gap-2'>
-              <IconBrandTiktok />{' '}
-              <span className='text-custom-lightgray/80 text-xs md:text-lg'>
-                Social Media
-              </span>
-            </li>
-            <li className='flex items-center gap-2 w-[200px]'>
-              <IconBrandWhatsapp />{' '}
-              <span className='text-custom-lightgray/80 text-xs md:text-lg'>
-                Social Media
-              </span>
-            </li>
+            {social.map((item, index) => (
+              <li key={index} className='flex items-center gap-2'>
+                {item.icon}{' '}
+                <span className='text-custom-lightgray/80 text-xs md:text-lg'>
+                  {item.text}
+                </span>
+              </li>
+            ))}
           </ol>
         </div>
 
         <div className='col-span-8 xs:col-span-4 md:col-span-2 space-y-4 text-start xs:text-end md:text-start'>
           <h3 className='text-xl font-bold'> {content('contact.title')}</h3>
           <ol className='grid grid-cols-4 gap-4'>
-            <li className='col-span-4 flex items-center xs:justify-end md:justify-start gap-2'>
-              <IconBrandWhatsapp />
-              <span className=' text-custom-lightgray/80 text-xs md:text-base'>
-                +57 326 251 1515
-              </span>
-            </li>
-            <li className='col-span-4'>
-              <span className=' text-custom-lightgray/80 text-xs md:text-base'>
-                +57 326 251 1515
-              </span>
-            </li>
-            <li className='col-span-4'>
-              <span className=' text-custom-lightgray/80 text-xs md:text-base'>
-                +57 326 251 1515
-              </span>
-            </li>
+            {phonesNumbers.map((phone, index) => (
+              <li key={index} className='col-span-4'>
+                <span className='text-custom-lightgray/80 text-xs md:text-base'>
+                  {phone.number}
+                </span>
+              </li>
+            ))}
           </ol>
         </div>
 
@@ -79,16 +49,13 @@ const Footer = () => {
           <h3 className='text-xl font-bold'>{content('visit.title')}</h3>
 
           <ol className='space-y-6'>
-            <li>
-              <p className='text-custom-lightgray/80 text-xs md:text-base'>
-                Crra 42 #### San isidro
-              </p>
-            </li>
-            <li>
-              <p className='text-custom-lightgray/80 text-xs md:text-base'>
-                Crra 42 #### San isidro
-              </p>
-            </li>
+            {streets.map((street, index) => (
+              <li key={index}>
+                <p className='text-custom-lightgray/80 text-xs md:text-base'>
+                  {street.street}
+                </p>
+              </li>
+            ))}
           </ol>
         </div>
       </div>
