4c2410f1a75b0547dd47ac7ec8e67666c1ab7756
Miguel AJM <koekun3000@gmail.com>
2024-05-11 16:40:08 -0500

    Configurando las variables de entorno

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -3,13 +3,13 @@ import { initializeApp } from 'firebase/app'
 import { getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCw9_KUEQpYUvVoyW45MKUZpt0GTPVSikQ',
-  authDomain: 'memorial-campionato-bd0ed.firebaseapp.com',
-  projectId: 'memorial-campionato-bd0ed',
-  storageBucket: 'memorial-campionato-bd0ed.appspot.com',
-  messagingSenderId: '3991882952',
-  appId: '1:3991882952:web:1bdbe2e80fdd69f356e9fd',
-  measurementId: 'G-4FPZJEQLL8'
+  apiKey: process.env.NEXT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_APP_FIREBASE_MEASUREMENT_ID
 }
 
 // Initialize Firebase
