4cb5b5aeaecafc762604f7ae87aeffce86c654cf
Orbitix Solutions <162249501+OrbitixSolutions@users.noreply.github.com>
2024-06-07 11:25:40 +0200

    Update layout.tsx

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -8,9 +8,9 @@ import '../globals.css'
 const DM_SANS_FONT = DM_Sans({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Ermirblushaj',
+  title: 'Torneo EB - Memorial Ermir Ermirblushaj',
   description:
-    "Ogni anno, Parma celebra con un campionato annuale il ricordo di Ermir Blushaj, una personalità significativa per la città. Questo evento non è solo una competizione, ma un'occasione per rafforzare i valori di comunità, integrità e amicizia."
+    "Ogni anno, Parma celebra con un campionato annuale (Torneo EB) il ricordo di Ermir Blushaj, una personalità significativa per la città. Questo evento non è solo una competizione, ma un'occasione per rafforzare i valori di comunità, integrità e amicizia."
 }
 
 export default function RootLayout({
